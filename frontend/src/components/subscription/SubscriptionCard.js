1487868466e7c2b9bcd7b30556a5561f0828b773
Adith <adithdinesh97@gmail.com>
2021-03-11 22:11:43 +0530

    [UPDATE] Integrated POST route for subscriptions

--- frontend/src/components/subscription/SubscriptionCard.js
+++ frontend/src/components/subscription/SubscriptionCard.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 export default function SubscriptionCard() {
+  const [email, setEmail] = useState('');
+
+  const submit = async () => {
+    try {
+      await Axios.post('/api/newsy/', {
+        email,
+        date: new Date().toISOString(),
+      });
+      alert('Successfully added!');
+    } catch (error) {
+      console.log(error.message);
+      alert('Something went wrong! Try again later.');
+    }
+  };
+
   return (
     <div className="flex flex-col h-96 w-full max-w-screen-sm md:shadow-xl  text-gray-800 uppercase">
       <div className="h-full flex flex-col justify-center items-center  px-5">
         <p className="text-center text-blue-200">
-          <i class="fas fa-envelope-open-text text-7xl "></i>
+          <i className="fas fa-envelope-open-text text-7xl "></i>
         </p>
         <h3 className="text-gray-500 text-center font-bold p-2">
           don't miss any update
@@ -17,11 +33,16 @@ export default function SubscriptionCard() {
       <div className="mt-auto pb-20 px-5">
         <div className="flex justify-center items-center">
           <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="outline-none bg-gray-100 p-3 w-80 mb-4 text-center rounded-none"
             placeholder="Enter your email address"
           />
-          <div className="flex justify-center items-center bg-gray-100 mb-4 p-4 outline-none rounded-none border-none text-gray-500 cursor-pointer hover:text-gray-700">
-            <i class="fas fa-paper-plane"></i>
+          <div
+            onClick={submit}
+            className="flex justify-center items-center bg-gray-100 mb-4 p-4 outline-none rounded-none border-none text-gray-500 cursor-pointer hover:text-gray-700"
+          >
+            <i className="fas fa-paper-plane"></i>
           </div>
         </div>
         <p className="text-gray-300 text-center text-xs">
